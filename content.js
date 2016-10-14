5ccc14e94f1e5c2e7dd3b9ac3929cd1b1fc1c55b
brnbp <bruno9pereira@gmail.com>
2016-10-14 20:41:37 -0300

    initial commit

---
+++ content.js
@@ -0,0 +1,42 @@
+
+(function() { 
+
+    const currentHour = new Date().getHours()
+
+    showLoader(true)
+
+    $('#title h2 span').text(getPartOfDay(currentHour))
+
+    $.getJSON('http://ipinfo.io', function (body) {
+        const city = body.city || 'Sao Paulo'
+        
+        if (!city) {
+            return
+        }
+
+        const url = 'http://api.openweathermap.org/data/2.5/weather?appid=a3ed1549cd8e7a34bd6148a5d1e56a5e&q='+ encodeURIComponent(city) +'&units=metric';
+      
+        $.getJSON(url, function (body) {
+          showLoader(false)
+          $('#title h2').append(' in ' + city + ':')
+          $('#weather').text("It's " + Math.floor(body.main.temp) + "c and " + body.weather[0].description)
+        })
+    })
+
+})()
+
+function getPartOfDay(hour) {
+    if (hour > 12 && hour < 18) {
+        return 'afternoon'
+    } else if( hour > 18 && hour < 6) {
+        return 'night'
+    }
+
+    return 'morning';
+}
+
+
+function showLoader(flag)
+{
+    $('#loading').attr('hidden', !flag);
+}
